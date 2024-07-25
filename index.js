a6065950b255927506ac74fdbb91b4c21e6ca34f
Rgains <researchgainsCopy@gmail.com>
2024-07-25 14:51:14 +0100

    class 5

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require("express");
+const { default: mongoose } = require("mongoose");
+
+const authRoutes = require("./routes/auth");
+
+require("dotenv").config();
 
 const app = express();
 
 const port = 11000;
 
-app.get("/api/v1/ijan-authentication", (req, res) => {
+app.use("/api/v1/ijan/authenticate", authRoutes);
+
+app.get("/api/v1/ijan", (req, res) => {
   return res.send("Hello!....Welcome to Backend Engineering at IJAN!");
 });
 
-app.listen(port, () => {
-  console.log("The server is currently running on port:", port);
+mongoose.connect(process.env.MONGO_URI).then((res) => {
+  if (res)
+    console.log("database connection successful:", process.env.MONGO_URI);
+  app.listen(port, () => {
+    console.log(`pay-only rest API is currently running on port ${port}`);
+  });
 });
--- ochael/ochael.js
+++
@@ -1,3 +0,0 @@
-const ochael = () => {
-  console.log("KAYODE!!!");
-};
